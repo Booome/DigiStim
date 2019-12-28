95878a2d0d9931fcadcbf1fc813cc5471e1b429d
BodongLiKolmostar <bodong.li@kolmostar.com>
2019-12-28 15:46:50 +0800

    add database & clean project

---
+++ GUI/database.h
@@ -0,0 +1,13 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <QObject>
+
+class DataBase : public QObject
+{
+    Q_OBJECT
+public:
+    explicit DataBase(QObject *parent = nullptr);
+};
+
+#endif // DATABASE_H
